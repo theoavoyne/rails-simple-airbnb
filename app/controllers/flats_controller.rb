309a08445e83cf88e38072d066c7185c4af92a26
Theo Avoyne <tavoyne@gmail.com>
2018-02-21 22:08:44 -0500

    flats#show view nice FE

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,17 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, only: [ :show ]
 
   def index
     @flats = Flat.all
+  end
+
+  def show
+  end
+
+  private
+
+  def find_flat
+    @id = params[:id]
+    @flat = Flat.find(@id)
   end
 end
